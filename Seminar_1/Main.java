8a8fd84d39932ebde0e44e853ac53499284919fd
Boris Shapovalov <bashap@bk.ru>
2023-01-20 00:28:15 +0300

    change program

--- Seminar_1/Main.java
+++ Seminar_1/Main.java
@@ -6,6 +6,8 @@ public class Main {
         vendingMachine.addProduct(new Product(20,"Хлеб"));
         vendingMachine.addProduct(new Product(42,"Масло Крестьянское"));
         vendingMachine.addProduct(new Chocolate(50,"Шоколад Аленка","Молочный с орехами"));
+        vendingMachine.addProduct(new Juice(12, "Сок", "Яблочный"));
+        vendingMachine.addProduct(new Eggs(9,"Яйца куриные", 12));
 
         for (Product item: vendingMachine.getProducts()) {
             System.out.println(item);
