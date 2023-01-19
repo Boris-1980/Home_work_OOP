1972f9f00b77649de45df4cdf756cfea21a43781
Boris Shapovalov <bashap@bk.ru>
2023-01-20 00:27:31 +0300

    add product

--- Seminar_1/Eggs.java
+++ Seminar_1/Eggs.java
@@ -1,1 +1,16 @@
 package Seminar_1;
+
+public class Eggs extends Product {
+    private int piece;
+
+    public Eggs(int price,String name, int piece){
+    super(price, name);
+    this.piece = piece;
+    }   
+
+    @Override
+    public String toString() {
+        return super.toString() + " штук " + piece;
+    }
+}
+
